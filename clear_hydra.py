9c4ad1759b75e8d0c51a17d3edb9f55684087e13
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-01-26 14:37:27 -0400

    add clear_hydra.py

---
+++ clear_hydra.py
@@ -0,0 +1,10 @@
+import os
+
+def clear_screen():
+    os.system('clear')
+
+def print_banner():
+    print('\033[0;34mClearHydra v0.1\033[0m')
+    print('Desenvolvidor por: Henrique Sebastião')
+    print('https://github.com/henriquesebastiao/ClearHydra')
+
